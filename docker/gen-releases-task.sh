8ba1e31096f5e65952fb51379326c7912013c00a
Alexandru Nedelcu <noreply@alexn.org>
2023-01-05 17:14:32 +0200

    Remove junk

--- docker/gen-releases-task.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] Generating releases.xml ..."
-mkdir -p "/opt/app/output/"
-exec java -jar "/opt/app/gen-releases.jar" >"/opt/app/output/releases.xml"
-echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] Done generating releases.xml!"
